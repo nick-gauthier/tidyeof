f982fe63927961e4c2b4e7c960cb08b335130094
Nick Gauthier <nicolas.gauthier@ufl.edu>
2025-09-29 11:22:35 -0400

    Update import_packages.R

--- R/import_packages.R
+++ R/import_packages.R
@@ -1,8 +1,8 @@
 #' @import stars dplyr broom tidyr stringr methods
 #' @importFrom sf st_crs
-#' @importFrom ggplot2 ggplot aes geom_line geom_hline geom_vline facet_wrap scale_fill_distiller coord_quickmap coord_sf theme_void theme_bw labs theme element_text unit margin
+#' @importFrom ggplot2 ggplot aes geom_line geom_hline geom_vline geom_col facet_wrap scale_fill_distiller coord_quickmap coord_sf theme_void theme_bw labs theme element_text unit margin
 #' @importFrom scico scale_fill_scico
 #' @importFrom glue glue glue_collapse
-#' @importFrom cli cli_h1 cli_h2 cli_text cli_progress_step cli_progress_done
+#' @importFrom cli cli_h1 cli_h2 cli_text cli_progress_step cli_progress_done cli_abort cli_inform
 #' @importFrom rlang abort caller_arg caller_env inherits_any
 NULL
